b397a704e6b2e458084ecca498bc1a9ff668b50f
Mazen-Hany-Abdelsalam-Hassan <113555799+Mazen-Hany-Abdelsalam-Hassan@users.noreply.github.com>
2025-02-27 14:24:39 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ EPOCHS =20
 MAX_SEQ_LENGTH = 10
 DEVICE = device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 GAUSSIAN_BLUR =  A.GaussianBlur(p=1, sigma_limit=(0 , 2))
-#SALT_AND_PAPER = A.SaltAndPepper(amount=(0.02, 0.1),salt_vs_pepper=(0.5, 0.5), p=.5)
+SALT_AND_PAPER = A.SaltAndPepper(amount=(0.02, 0.1),salt_vs_pepper=(0.5, 0.5), p=.5)
 GRID_DISTORTION =  A.GridDistortion(num_steps=5, distort_limit=0.5, p=.5)
 TRAIN_PROCESSING_PIPELINE = [GAUSSIAN_BLUR , SALT_AND_PAPER , GRID_DISTORTION, A.Normalize(), ToTensorV2()]
 TEST_PROCESSING_PIPELINE = [A.Normalize(), ToTensorV2()]
