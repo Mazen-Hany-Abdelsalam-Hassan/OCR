cb928e56f43d9eba2e56e3eba1f397a5360def29
Mazen-Hany-Abdelsalam-Hassan <113555799+Mazen-Hany-Abdelsalam-Hassan@users.noreply.github.com>
2025-03-03 13:46:29 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ DATA_DIR  = r'data/*.png'
 IMAGE_WIDTH = 180
 IMAGE_HEIGHT = 90
 EPOCHS =20
-MAX_SEQ_LENGTH = 10
+MAX_SEQ_LENGTH = 20
 DEVICE = device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 GAUSSIAN_BLUR =  A.GaussianBlur(p=1, sigma_limit=(0 , 2))
 SALT_AND_PAPER = A.SaltAndPepper(amount=(0.02, 0.1),salt_vs_pepper=(0.5, 0.5), p=.5)
